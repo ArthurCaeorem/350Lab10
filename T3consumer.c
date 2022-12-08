0b3e3a70832f586d28c416051ddbb8b5bd593c2c
ArthurCaeorem <65918714+ArthurCaeorem@users.noreply.github.com>
2022-12-07 22:37:50 -0500

    Update T3consumer.c

--- T3consumer.c
+++ T3consumer.c
@@ -1,1 +1,77 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/shm.h>
+#include <sys/sem.h>
+#include<errno.h>
+#define MUTEX 0
+#define FULL 1
+#define EMPTY 2
+#define NOT_READY -1
+#define FILLED 0 
+#define TAKEN 1 
+#define GO 2 
+#define STOP 3 
 
+//memory struct
+struct Memory {
+int status; 
+int gostop; 
+int i[5];
+int count;
+int Psleep;
+int Csleep;
+};
+
+/*
+program that simulates the producer-consumer problem utilizing both shared memory and semaphores.
+This process simulates the consumer
+*/
+
+//decrements semaphore value
+void down(int semid, int index)
+{
+struct sembuf buf = {index, -1, 0};
+semop(semid, &buf, 1);
+}
+//increments semaphore value
+void up(int semid, int index)
+{
+struct sembuf buf = {index, 1, 0};
+semop(semid, &buf, 1);
+}
+
+//union used for arg
+union semun {
+    int val;
+    struct sumid_ds *buf;
+    unsigned short *array;
+};
+
+int main(){
+union semun arg;
+key_t key;
+int ID,semID;
+struct Memory *shm;
+key = ftok(".",'G'); //key is retrieved
+ID = shmget(key,sizeof(struct Memory *),0666); //shared memory is retrieved
+shm = (struct Memory *) shmat(ID,NULL,0); //shared memory is accessed
+semID = semget(key,3,0666); //semaphores are retrieved
+
+int item=0;
+while(1){
+    down(semID,MUTEX); //mutex becomes blocked
+    down(semID,FULL);
+    for(int i = 0; i < 5; i++){
+    shm->count--; //decrement count
+    item = shm->i[shm->count]; //records this shm->i location as item
+    shm->i[shm->count] = 0;
+    printf("%d has been consumed.\n",item);
+    }
+    printf("consumer finished.\n");
+    sleep(3);
+    up(semID,MUTEX); //mutex is unblocked
+    up(semID,EMPTY);
+}
+    
+    exit(0);
+}
